5539f3a6a46bc28a8ecc215e0ed2de681e3f4623
LiefJamesCain <liefjamescain@gmail.com>
2019-03-07 15:28:20 -0800

    hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,20 +1,24 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+1. code is scanned for function declarations:
+    - for each function, a property is created in the Variable Object, pointing to the function.
+2. code is scanned for variable declarations:
+    - for each variable, a property is created in the Variable Object, and set to undefined.
+    */
 
+// functions 
+function calcAge(year) {
+    console.log(2018 - year);
+}
 
+var retire = function(year) {
+    console.log(65 - (2018 - year));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+// variables
+var age = 34; 
+console.log(age);
 
 
 ///////////////////////////////////////
