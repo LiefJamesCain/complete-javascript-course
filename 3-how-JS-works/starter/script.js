5705cfc38983b33699d2e27b36cac5fdb6ed9eea
LiefJamesCain <liefjamescain@gmail.com>
2019-03-09 10:20:21 -0800

    section 3 end

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -24,12 +24,13 @@ console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
-// scoping answers the question: where can we access this variable? 
+///////////////////////////////////////
+
+// scoping answers the question: where can we access this variable?
 // each new function declares a scope
-// lexical scoping: 
-//  - function that is lexically within another function gets access to the scope of  outer function
+// lexical scoping = function that is lexically within another function gets access to the scope of outer (parent) function and variables 
 
-
+/*
 var a = 'Hello!';
 first();
 
@@ -42,11 +43,12 @@ function first() {
         console.log(a + b + c);
     }
 }
+*/
+// locally scoped variables are not visible to their parents
 
 
 
-
-// Example to show the differece between execution stack and scope chain
+// Example: difference between execution stack & scope chain
 
 /*
 var a = 'Hello!';
@@ -62,9 +64,11 @@ function first() {
     }
 }
 
+// this function is obv outside the scope so it would not be able to access to b and c variables
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    // console.log(d);
+    console.log(a + d);
 }
 */
 
@@ -73,11 +77,46 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+/*
+- third and last step of creation phase 
+- regular function call
+    - keyword points at global Object
+- method call   
+    - this variable points to object calling the method
+- THIS variable is not assigned a value until a function where it is defined is called
+*/
 
+// calcAge(1984);
 
+// function calcAge(year) {
+//     console.log(2018 - year);
+//     console.log(this);
+// }
 
+var john = {
+    name: 'John',
+    yearBorn: 1984,
+    calcAge: function() {
+        console.log(this);
+        console.log(2018 - this.yearBorn);
 
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
 
+john.calcAge();
 
+var mike = {
+    name: 'Mike',
+    yearBorn: 1986
+};
 
+// this borrows the function from john and uses it on mike
+// works because of the 'this' keyword in the function
 
+mike.calcAge = john.calcAge;
+
+mike.calcAge();
